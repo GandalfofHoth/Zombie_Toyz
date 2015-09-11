6ede1f78c64703c97c25c3edf9de62b01d0d1f45
GandalfofHoth <npottle@IT302.mr.preble.com>
2015-09-11 12:08:03 -0400

    camera

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public Transform target;
+	public float smoothing = 5f;
+
+	Vector3 offset;
+
+	void Start()
+	{
+		offset = transform.position - target.position;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void FixedUpdate()
+	{
+		Vector3 targetCamPos = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing *Time.deltaTime);
 	}
 }
